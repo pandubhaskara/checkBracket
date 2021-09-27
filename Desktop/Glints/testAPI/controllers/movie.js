5e7a81c81c02d31f7375cd4466fbdd8cac89cde4
pandu bhaskara <pandubhaskara40@gmail.com>
2021-09-27 11:00:51 +0700

    controllers postmov

--- Desktop/Glints/testAPI/controllers/movie.js
+++ Desktop/Glints/testAPI/controllers/movie.js
@@ -1,9 +1,45 @@
+const Joi= require('joi')
 const {moviedb} = require("../models");
 
 module.exports = {
   postMovie: async (req, res) => {
     const body = req.body;
     try {
+      const schema = Joi.object({
+        title: Joi.string().required(),
+        synopsis: Joi.text().required(),
+        trailer: Joi.string().required(),
+        poster: Joi.string().required(),
+        rating: Joi.number().required(),
+        releaseDate: body.releaseDate,
+        director: Joi.string().required(),
+        featuredSong: Joi.string().required(),
+        budget: Joi.string().required(),
+      });
+
+      const { error } = schema.validate(
+        {
+        title: body.title,
+        synopsis: body.synopsis,
+        trailer: body.trailer,
+        poster: body.poster,
+        rating: body.rating,
+        releaseDate: body.releaseDate,
+        director: body.director,
+        featuredSong: body.featuredSong,
+        budget: body.budget,
+        },
+        { abortEarly: false }
+      );
+
+      if (error) {
+        return res.status(400).json({
+          status: "failed",
+          message: "Bad Request",
+          errors: error["details"][0]["message"],
+        });
+      }
+    
       const check = await moviedb.create({
         title: body.title,
         synopsis: body.synopsis,
@@ -34,7 +70,7 @@ module.exports = {
         message: "Internal Server Error",
       });
     }
-  },
+},
   getMovie: async (req, res) => {
     try {
       const data = await moviedb.findAll();
