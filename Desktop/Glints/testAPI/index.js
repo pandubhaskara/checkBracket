1bde0af6af54a84759d7fec475ba315b555940b3
pandu bhaskara <pandubhaskara40@gmail.com>
2021-09-27 09:05:41 +0700

    ganti index

--- Desktop/Glints/testAPI/index.js
+++ Desktop/Glints/testAPI/index.js
@@ -1,19 +1,18 @@
-var require= NodeRequire;
 const express = require("express")
 const app = express()
-const router= require('./routes')
 
-const input = require('./data.json')
-let port = process.env.PORT || 5432;
+
+// const input = require('./data.json')
+let port = process.env.PORT || 5000;
 
 app.use(express.json())
 app.get("/", (req,res)=>{
     res.send("hello World")
 })
-app.use("/api", router)
-app.get("/players", (req,res)=>{
-    res.send(input)
-})
+app.use("/api", require("./routes/index"))
+// app.get("/players", (req,res)=>{
+//     res.send(input)
+// })
 
 app.listen(port, ()=>{
     console.log(`Server running on port ${port}`)
